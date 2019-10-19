331962cd5a58b08b4a25b3b9f9592c7b76b24c70
Yusuke Sato <yusuke.sato@ys-office.me>
2019-10-20 01:44:10 +0900

    「アイコンの設定」まで完了

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
   <v-app>
+    <v-navigation-drawer app v-model="drawer" clipped>Navigation Lists</v-navigation-drawer>
+    <v-app-bar color="primary" dark app clipped-left>
+      <v-app-bar-nav-icon @click="drawer =! drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text>For Enterprise</v-btn>
+        <v-menu offset-y>
+          <template v-slot:activator="{on}">
+            <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+          </template>
+            <v-list>
+              <v-subheader>Get help</v-subheader>
+                <v-list-item v-for="support in supports" :key="support.name">
+                  <v-list-item-icon>
+                  <v-icon>{{ support.icon }}</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                  <v-list-item-title>{{ support.name }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-menu>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 
 export default {
-
-};
+  data () {
+    return {
+      drawer: false,
+      supports: [
+        { name: 'Consulting and suppourt', icon: 'mdi-vuetify' },
+        { name: 'Discord community', icon: 'mdi-discord' },
+        { name: 'Report a bug', icon: 'mdi-bug' },
+        { name: 'Github issue board', icon: 'mdi-github-face' },
+        { name: 'Stack overview', icon: 'mdi-stack-overflow' }
+      ]
+    }
+  }
+}
 </script>
