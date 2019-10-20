b0111c576cd2e5727e47a742552eac3be6039d73
Yusuke Sato <yusuke.sato@ys-office.me>
2019-10-20 12:47:15 +0900

    「Nestedリストの設定」まで完了

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,26 @@
           </v-list-item-content>
         </v-list-item>
         <v-divider></v-divider>
-        <v-list dense nav>
-          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
-            <v-list-item-icon>
-              <v-icon>{{ nav_list.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
+          <v-list nav dense>
+            <v-list-group
+              v-for="nav_list in nav_lists"
+              :key="nav_list.name"
+              :prepend-icon="nav_list.icon"
+              no-action
+              :append-icon="nav_list.lists ? undefined : ''"
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item v-for="list in nav_list.lists" :key="list">
+                <v-list-item-content>
+                  <v-list-item-title>{{ list }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+          </v-list>
       </v-container>
     </v-navigation-drawer>
     <v-app-bar color="primary" dark app clipped-left>
@@ -67,12 +77,33 @@ export default {
         { name: 'Stack overview', icon: 'mdi-stack-overflow' }
       ],
       nav_lists: [
-        { name: 'Getting Started', icon: 'mdi-vuetify' },
-        { name: 'Customization', icon: 'mdi-cogs' },
-        { name: 'Styles & animations', icon: 'mdi-palette' },
-        { name: 'UI Components', icon: 'mdi-view-dashboard' },
-        { name: 'Directives', icon: 'mdi-function' },
-        { name: 'Preminum themes', icon: 'mdi-vuetify' }
+        {
+          name: 'Getting Started',
+          icon: 'mdi-speedometer',
+          lists: ['Quick Start', 'Pre-made layouts']
+        },
+        {
+          name: 'Customization',
+          icon: 'mdi-cogs'
+        },
+        {
+          name: 'Styles & animations',
+          icon: 'mdi-palette',
+          lists: ['Colors', 'Content', 'Display']
+        },
+        {
+          name: 'UI Components',
+          icon: 'mdi-view-dashboard',
+          lists: ['API explorer', 'Alerts']
+        },
+        {
+          name: 'Directives',
+          icon: 'mdi-function'
+        },
+        {
+          name: 'Preminum themes',
+          icon: 'mdi-vuetify'
+        }
       ]
     }
   }
