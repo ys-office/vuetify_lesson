393c1aec597c28ee03419f64199cc5dd0503a517
Yusuke Sato <yusuke.sato@ys-office.me>
2019-10-20 12:37:46 +0900

    「ナビゲーションメニューの中身の設定」まで完了

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer" clipped>Navigation Lists</v-navigation-drawer>
+      <v-navigation-drawer app v-model="drawer" clipped >
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title grey--text text--darken-2">
+              Navigation lists
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list dense nav>
+          <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+            <v-list-item-icon>
+              <v-icon>{{ nav_list.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
     <v-app-bar color="primary" dark app clipped-left>
       <v-app-bar-nav-icon @click="drawer =! drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
@@ -37,13 +58,21 @@
 export default {
   data () {
     return {
-      drawer: false,
+      drawer: true,
       supports: [
         { name: 'Consulting and suppourt', icon: 'mdi-vuetify' },
         { name: 'Discord community', icon: 'mdi-discord' },
         { name: 'Report a bug', icon: 'mdi-bug' },
         { name: 'Github issue board', icon: 'mdi-github-face' },
         { name: 'Stack overview', icon: 'mdi-stack-overflow' }
+      ],
+      nav_lists: [
+        { name: 'Getting Started', icon: 'mdi-vuetify' },
+        { name: 'Customization', icon: 'mdi-cogs' },
+        { name: 'Styles & animations', icon: 'mdi-palette' },
+        { name: 'UI Components', icon: 'mdi-view-dashboard' },
+        { name: 'Directives', icon: 'mdi-function' },
+        { name: 'Preminum themes', icon: 'mdi-vuetify' }
       ]
     }
   }
